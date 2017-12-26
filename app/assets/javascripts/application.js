b8f7ac9f207fd46a4d407c44cb867575297a0a6a
Brisingr <mdavid.almiron@gmail.com>
2017-12-26 17:55:42 -0300

    Agregado jquery-validate

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,10 +11,29 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require jquery
+//= require jquery3
 //= require popper.min
 //= require unify/jquery-migrate.min
 //= require unify/jquery.easing.min
 //= require unify/bootstrap
+//= require hs.megamenu
+//= require unify/hs.core
+//= require unify/hs.header
+//= require unify/hs.hamburgers
+//= require unify/jquery-validate/jquery.validate.min
+//= require unify/jquery-validate/messages_es
 //= require turbolinks
 
+
+    $(window).on('load', function () {
+      // initialization of header
+      $.HSCore.components.HSHeader.init($('#js-header'));
+      $.HSCore.helpers.HSHamburgers.init('.hamburger');
+
+      // initialization of HSMegaMenu component
+      $('.js-mega-menu').HSMegaMenu({
+        event: 'hover',
+        pageContainer: $('.container'),
+        breakpoint: 991
+      });
+    });
--- app/models/persona.rb
+++ app/models/persona.rb
@@ -1,6 +1,8 @@
 class Persona < ApplicationRecord
 	belongs_to :empresa
 
+	#validaciones
+
 	# Inficamos que usa gema carrierwave para subir los pdf, hay que indicar los campos que lo usan
 	mount_uploader :pdf_dni_path, PdfUploader
 	mount_uploader :pdf_cuil_path, PdfUploader
